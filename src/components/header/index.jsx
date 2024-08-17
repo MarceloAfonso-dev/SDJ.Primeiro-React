cbe054cb0d6fcaf4df72677fb131d751376b55f4
Marcelo Afonso <dev.marceloafonso@gmail.com>
2024-08-17 16:03:50 -0300

    Começando repositório e projeto React

---
+++ src/components/header/index.jsx
@@ -0,0 +1,16 @@
+import './styles.css'
+export default function Header() {
+    return(
+        <header>
+            <div className="logo-container">
+                <img src="" alt="Logo" />
+            </div>
+            <nav>
+                <a href="">Link One</a>
+                <a href="">Link Two</a>
+                <a href="">Link Three</a>
+                <a href="">Link Four</a>
+            </nav>
+        </header>
+    );
+}

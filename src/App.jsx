8d4364564924c9408ccc452a6c043e6252816dcf
Marcelo Afonso <dev.marceloafonso@gmail.com>
2024-08-24 14:22:27 -0300

    Children, joga qualquer elemento que vira filho nele

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import Card from "./components/card";
-
+function Filho({ children }) {
+  return <div className="filho">{ children }</div>
+}
 
 export default function App() {
-  return(
-    <Card 
-    title="Cartão Visa 0976" 
-    image="https://pontospravoar.com/wp-content/uploads/2022/09/click_carta%CC%83o-digital.png"
-    description="Limite disponível: R$ 1.000,00"
-  />
-  )
+  return <Filho>
+    <p>Sou uma tag p passando por children</p>
+  </Filho>
 }

621a342a11c499cc9f7620b446365506d932e3ed
Marcelo Afonso <dev.marceloafonso@gmail.com>
2024-08-24 15:40:20 -0300

    uso da props boolean

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,33 @@
 // function Filho({ idade }) {
 //  return <p>{idade}</p>
 //}
-
+function Imagem({url, alt, descricao}) {
+  return (
+<div>
+  <img src={url} alt={alt}  /> <p>{descricao}</p>
+</div>
+);
+}
 function Filho({ approved }) {
-  return(
-  <>
-    {approved ? <p>aprovado</p> : <p>reprovado</p>}
-  </>)
-  }
+  return (
+    <>
+      {approved ? (
+        <Imagem
+          url="https://png.pngtree.com/png-clipart/20230810/original/pngtree-happy-emoji-emoticon-showing-double-thumbs-up-like-picture-image_7851906.png"
+          alt="feliz"
+          descricao="Aprovado"
+        />
+      ) : (
+        <Imagem
+          url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeNDGyBOFW7PsmFcRicnODeaP6bwT-iXw4mg&s"
+          alt="triste"
+          descricao="Reprovado"
+        />
+      )}
+    </>
+  );
+}
 
 export default function App() {
-  return <Filho approved />
+  return <Filho approved={false} />
 }

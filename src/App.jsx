bb07800a1a8863cc7a25ce0ac29fd10c50d58e1c
Marcelo Afonso <dev.marceloafonso@gmail.com>
2024-08-24 15:50:29 -0300

    filho de botão em alerta

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,7 @@
-//function Filho({ children }) {
-//   return <div className="filho">{ children }</div>
-// }
-
-// function Filho({ idade }) {
-//  return <p>{idade}</p>
-//}
-function Imagem({url, alt, descricao}) {
-  return (
-<div>
-  <img src={url} alt={alt}  /> <p>{descricao}</p>
-</div>
-);
-}
-function Filho({ approved }) {
-  return (
-    <>
-      {approved ? (
-        <Imagem
-          url="https://png.pngtree.com/png-clipart/20230810/original/pngtree-happy-emoji-emoticon-showing-double-thumbs-up-like-picture-image_7851906.png"
-          alt="feliz"
-          descricao="Aprovado"
-        />
-      ) : (
-        <Imagem
-          url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeNDGyBOFW7PsmFcRicnODeaP6bwT-iXw4mg&s"
-          alt="triste"
-          descricao="Reprovado"
-        />
-      )}
-    </>
-  );
+function Filho({ onAlert }) {
+  return <button onClick={onAlert}>Receba seu alert</button>;
 }
 
 export default function App() {
-  return <button onClick={() => alert("função funcionando")}>Click Aqui</button>;
+  return Filho({ onAlert: () => alert('Alerta funcionando!') });
 }

42a9b09da858a56f2ba310ecf5675ca5b8a63787
Marcelo Afonso <dev.marceloafonso@gmail.com>
2024-08-31 15:54:13 -0300

    feat: alterador de classe a partir do pai como props, classe dinamica, mas se nao existir, fica no padrão, não muda nada

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import Brand from "./components/brand";
+import Square from "./components/square";
 
 export default function App() {
   return (
-    <div>
-      <Brand brand="4111111111111111" />
-      <Brand brand="5555555555554444" />
-      <Brand brand="378282246310005" />
-      <Brand brand="5067999999999999" />
-      <Brand brand="" />
+    <div className="app-container">
+      <Square color="red" />
     </div>
   );
 }

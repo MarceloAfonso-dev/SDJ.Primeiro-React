c6bb459d70cccb6a487a2b76db7ef41d4ccff3d1
Marcelo Afonso <dev.marceloafonso@gmail.com>
2024-09-14 14:58:53 -0300

    feat: api pokemon 2

--- src/class/hooks/useHooks/pokemonAPI/index.jsx
+++ src/class/hooks/useHooks/pokemonAPI/index.jsx
@@ -1,12 +1,16 @@
 import { useEffect } from 'react';
+import { useState } from 'react';
 
 export default function PokemonAPI() {
+    const [pokemon, setPokemon] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
-        .then((res) => console.log(res.json()),"res")
+        fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+        .then((res) => console.log(res.json()))
         .then((data) => console.log(data) ,"data")
-        .catch((error) =>  console.log(error) );
+        .catch((error) =>  console.log(error), "error" );
     }, []);
 
     return (
